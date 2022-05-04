fd0de1ed5cef2106605343d73e3645579f2cc2eb
spockele <j.s.pockele@student.tudelft.nl>
2022-05-04 12:30:16 +0200

    Added some reset functions to the skeleton for later

--- Lifting_Line.py
+++ Lifting_Line.py
@@ -7,6 +7,9 @@ class ControlPoint:
         self.theta = theta
         self.z = z
 
+        self.x0 = x0
+        self.y0 = y0
+
         self.x = r * np.cos(theta) + x0
         self.y = r * np.sin(theta) + y0
 
@@ -17,22 +20,41 @@ class ControlPoint:
         self.circulation = magnitude
         self.orientation = orientation
 
+    def reset(self):
+        self.__init__(self.r, self.theta, self.z, self.x0, self.y0)
+
 
 class Leg:
-    def __init__(self):
-        self.control_points = []
+    def __init__(self, reset=False):
+        if not reset:
+            self.control_points = []
 
         self.circulation = None
+
+    def reset(self):
+        [cp.reset() for cp in self.control_points]
+        self.__init__(reset=True)
 
 
 class HorseShoe:
-    def __init__(self):
-        self.leg1 = Leg()
-        self.leg2 = Leg()
+    def __init__(self, reset=False):
+        if not reset:
+            self.leg1 = Leg()
+            self.leg2 = Leg()
 
         self.circulation = None
 
+    def reset(self):
+        self.leg1.reset()
+        self.leg2.reset()
+        self.__init__(reset=True)
+
 
 class Wake:
-    def __init__(self):
-        self.horse_shoes = []
+    def __init__(self, reset=False):
+        if not reset:
+            self.horse_shoes = []
+
+    def reset(self):
+        [hs.reset() for hs in self.horse_shoes]
+        self.__init__(reset=True)

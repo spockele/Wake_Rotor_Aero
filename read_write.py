51d33d8bb32fc8fde216710e42f02169e536b322
spockele <j.s.pockele@student.tudelft.nl>
2022-05-11 12:18:44 +0200

    Added read and write function as its own module.

---
+++ read_write.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def write_to_file(array, path):
+    lines = []
+    for row in array:
+        line = ''
+        for num in row:
+            line = line + f'{num},'
+
+        lines.append(line[:-1] + '\n')
+
+    f = open(path, 'w')
+    f.writelines(lines)
+    f.close()
+
+
+def read_from_file(path):
+    f = open(path)
+    lines = f.readlines()
+    out_list = [[float(num) for num in line.strip('\n').split(',')] for line in lines]
+    return np.array(out_list)

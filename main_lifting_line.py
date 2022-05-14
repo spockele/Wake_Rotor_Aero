36f60b61f8c7207822aac15fd9334172d9d87e42
spockele <j.s.pockele@student.tudelft.nl>
2022-05-14 10:13:50 +0200

    Put the main running thing in a nice function

--- main_lifting_line.py
+++ main_lifting_line.py
@@ -1,59 +1,65 @@
 import matplotlib.pyplot as plt
-
 from Lifting_Line import *
-# Ugly but it worksss
 
-mainTurbine = Turbine(0, (0,0,0))
 
-# This file contains the main iteration over two turbines.
-maxiterations = 100
-iteri = 1
-bConverged = False
-highestIndex, highestDeltaGamma = mainTurbine.set_circulations_horseshoes()
-print(f"Initial calculation. max delta gamma = {highestDeltaGamma} at idx = {highestIndex}")
-while iteri < maxiterations:
-    mainTurbine.SetInducedVelocityForHorseshoes()
+def run_lifting_line():
+    # Ugly but it worksss
+
+    mainTurbine = Turbine(0, (0,0,0))
+
+    # This file contains the main iteration over two turbines.
+    maxiterations = 100
+    iteri = 1
+    bConverged = False
     highestIndex, highestDeltaGamma = mainTurbine.set_circulations_horseshoes()
+    print(f"Initial calculation. max delta gamma = {highestDeltaGamma} at idx = {highestIndex}")
+    while iteri < maxiterations:
+        mainTurbine.SetInducedVelocityForHorseshoes()
+        highestIndex, highestDeltaGamma = mainTurbine.set_circulations_horseshoes()
 
 
 
-    # print ("Finished iteration {}. max delta gamma = {:.3f}".format(iteri, highestDeltaGamma))
-    print(f"Finished iteration {iteri}. max delta gamma = {highestDeltaGamma} at idx = {highestIndex}")
-    # Exit criterion based on change in delta gamma
-    if abs(highestDeltaGamma) < 1e-1:
-        bConverged = True
-        break;
+        # print ("Finished iteration {}. max delta gamma = {:.3f}".format(iteri, highestDeltaGamma))
+        print(f"Finished iteration {iteri}. max delta gamma = {highestDeltaGamma} at idx = {highestIndex}")
+        # Exit criterion based on change in delta gamma
+        if abs(highestDeltaGamma) < 1e-1:
+            bConverged = True
+            break;
 
-    iteri += 1
+        iteri += 1
 
-print("Done :D")
+    print("Done :D")
 
-plt.figure(1)
-out = mainTurbine.extract_information_N_write()
-plt.plot(out[0, 0], np.degrees(out[0, 1]), label='alpha 0')
-plt.plot(out[0, 0], np.degrees(out[0, 2]), label='phi 0')
-plt.plot(out[1, 0], np.degrees(out[1, 1]), label='alpha 1')
-plt.plot(out[1, 0], np.degrees(out[1, 2]), label='phi 1')
-plt.plot(out[2, 0], np.degrees(out[2, 1]), label='alpha 2')
-plt.plot(out[2, 0], np.degrees(out[2, 2]), label='phi 2')
-plt.legend()
+    plt.figure(1)
+    out = mainTurbine.extract_information_N_write()
+    plt.plot(out[0, 0], np.degrees(out[0, 1]), label='alpha 0')
+    plt.plot(out[0, 0], np.degrees(out[0, 2]), label='phi 0')
+    plt.plot(out[1, 0], np.degrees(out[1, 1]), label='alpha 1')
+    plt.plot(out[1, 0], np.degrees(out[1, 2]), label='phi 1')
+    plt.plot(out[2, 0], np.degrees(out[2, 1]), label='alpha 2')
+    plt.plot(out[2, 0], np.degrees(out[2, 2]), label='phi 2')
+    plt.legend()
 
-plt.figure(2)
-plt.plot(out[0, 0], out[0, 3], label='pn 0')
-plt.plot(out[0, 0], out[0, 4], label='pt 0')
-plt.plot(out[1, 0], out[1, 3], label='pn 1')
-plt.plot(out[1, 0], out[1, 4], label='pt 1')
-plt.plot(out[2, 0], out[2, 3], label='pn 2')
-plt.plot(out[2, 0], out[2, 4], label='pt 2')
-plt.legend()
+    plt.figure(2)
+    plt.plot(out[0, 0], out[0, 3], label='pn 0')
+    plt.plot(out[0, 0], out[0, 4], label='pt 0')
+    plt.plot(out[1, 0], out[1, 3], label='pn 1')
+    plt.plot(out[1, 0], out[1, 4], label='pt 1')
+    plt.plot(out[2, 0], out[2, 3], label='pn 2')
+    plt.plot(out[2, 0], out[2, 4], label='pt 2')
+    plt.legend()
 
-plt.figure(3)
-plt.plot(out[0, 0], out[0, 5], label='a 0')
-plt.plot(out[0, 0], out[0, 6], label='ap 0')
-plt.plot(out[1, 0], out[1, 5], label='a 1')
-plt.plot(out[1, 0], out[1, 6], label='ap 1')
-plt.plot(out[2, 0], out[2, 5], label='a 2')
-plt.plot(out[2, 0], out[2, 6], label='ap 2')
-plt.legend()
+    plt.figure(3)
+    plt.plot(out[0, 0], out[0, 5], label='a 0')
+    plt.plot(out[0, 0], out[0, 6], label='ap 0')
+    plt.plot(out[1, 0], out[1, 5], label='a 1')
+    plt.plot(out[1, 0], out[1, 6], label='ap 1')
+    plt.plot(out[2, 0], out[2, 5], label='a 2')
+    plt.plot(out[2, 0], out[2, 6], label='ap 2')
+    plt.legend()
 
-plt.show()
+    plt.show()
+
+
+if __name__ == '__main__':
+    run_lifting_line()

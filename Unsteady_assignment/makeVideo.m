4a90fa897b91c29b72e2e9f11e289270dd33688a
yannbirnie <64381239+yannbirnie@users.noreply.github.com>
2022-06-26 20:31:49 +0200

    plots

--- Unsteady_assignment/makeVideo.m
+++ Unsteady_assignment/makeVideo.m
@@ -1,12 +1,21 @@
-folderName = './figures/field';
-
+clear; 
+%% UNSTEADY
+folderName = './figures/field/unsteady/K_0.10';
 % load the images
 folder = dir([folderName '/*.jpg']);
 N = size(folder,1);
 images = cell(N,1);
 for ii=1:N; images{ii} = imread(strcat(folderName,sprintf('/%.0f.jpg',ii)));    end
-% create the video writer with # fps
- writerObj = VideoWriter('field.avi');
+
+% %% STEADY
+% folderName = './figures/field/steady';
+% load(strcat(folderName,'/subplot_figList.mat'));
+% N = size(textList,1);
+% images = cell(N,1);
+% for ii=1:N; images{ii} = imread(textList{ii});    end
+
+%% create the video writer with # fps
+ writerObj = VideoWriter(strcat(folderName,'video.avi'));
  writerObj.FrameRate = 2;
  % set the seconds per image
  secsPerImage = ones(1,N);
